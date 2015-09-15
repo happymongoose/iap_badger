3ecba8fa33ae085249ec5b5ddc37644cd842c8a2
happymongoose <admin@Simons-iMac.local>
2015-09-15 08:04:25 +0100

    Updated example code
    Example 1 code updated to include restore functions

--- example 1/main.lua
+++ example 1/main.lua
@@ -2,6 +2,23 @@
 --Example1.lua
 --
 --Simple example of using IAP Badger to purchase an IAP for removing advertisements from an app.
+--Also includes a restore products function
+
+---------------------------------
+-- 
+-- Declarations
+--
+---------------------------------
+
+--Progress spinner
+local spinner=nil
+--Buy button group
+local buyGroup=nil
+--Advertisment group
+local adGroup=nil
+
+--Forward declaration for buyUnlock function
+local buyUnlock=nil
 
 
 ---------------------------------
@@ -12,16 +29,6 @@
 
 --Load IAP Badger
 local iap = require("iap_badger")
-
---Progress spinner
-local spinner=nil
---Buy button group
-local buyGroup=nil
---Advertisment group
-local adGroup=nil
-
---Forward declaration
-local buyUnlock=nil
 
 --Create the catalogue
 local catalogue = {
@@ -49,9 +56,8 @@ local catalogue = {
     }
 }
 
---Called when a purchase fails
+--Called when any purchase fails
 local function failedListener()
-    
     --If the spinner is on screen, remove it
     if (spinner) then 
         spinner:removeSelf()
@@ -60,9 +66,9 @@ local function failedListener()
     
 end
 
-
+--This table contains all of the options we need to specify in this example program.
 local iapOptions = {
-	--The catalogue generated above
+    --The catalogue generated above
     catalogue=catalogue,
     --The filename in which to save the inventory
     filename="example1.txt",
@@ -73,29 +79,48 @@ local iapOptions = {
     failedListener=failedListener,
     cancelledListener=failedListener,
     --Once the product has been purchased, it will remain in the inventory.  Uncomment the following line
-    --to test the purchase functions again in future.
+    --to test the purchase functions again in future.  It's also useful for testing restore purchases.
     --doNotLoadInventory=true
 }
 
 --Initialise IAP badger
 iap.init(iapOptions)
 
+---------------------------------
+-- 
+-- Making purchases
+--
+---------------------------------
 
+--The functionality for removing the ads from the screen has been put in a separate
+--function because it will be called from the purchaseListener and the restoreListener
+--functions
+local function removeAds()    
+    --Remove the advertisement (need to check it's there first - if this function
+    --is called from a product restore, it may not have been created)
+    if (adGroup) then
+        adGroup:removeSelf()
+        adGroup=nil
+    end
+    --Change the button text
+    buyGroup.text.text="Game unlocked"
+    buyGroup:removeEventListener("tap", buyUnlock)
+end
+
+--Called when the relevant app store has completed the purchase
 local function purchaseListener(product )
     --Remove the spinner
     spinner:removeSelf()
     spinner=nil
-    --Remove the advertisement
-    adGroup:removeSelf()
-    adGroup=nil
-    --Change the button text
-    buyGroup.text.text="Game unlocked"
-    buyGroup:removeEventListener("tap", buyUnlock)
+    --Remove the ads
+    removeAds()
     --Save the inventory change
     iap.saveInventory()
 end
 
 --Purchase function
+--Most of the code in this function places a spinner on screen to prevent any further user interaction with
+--the screen.  The actual code to initiate the purchase is the single line iap.purchase("removeAds"...)
 buyUnlock=function()
     
     --Place a progress spinner on screen and tell the user the app is contating the store
@@ -116,9 +141,69 @@ buyUnlock=function()
     spinner:insert(spinnerRect)
     
     --Tell IAP to initiate a purchase
-    iap.purchase("removeAds", purchaseListener)
+    iap.purchase("removeAds", purchaseListener, failedListener)
     
 end
+
+---------------------------------
+-- 
+-- Restoring purchases
+--
+---------------------------------
+
+local function restoreListener(productName, event)
+    
+    --If this is the first product to be restored, remove the spinner
+    --(Not really necessary in a one-product app, but I'll leave this as template
+    --code for those of you writing apps with multi-products).
+    if (event.firstRestoreCallback) then
+        --Remove the spinner from the screen
+        spinner:removeSelf()
+        spinner=nil        
+        --Tell the user their items are being restore
+        native.showAlert("Restore", "Your items are being restored", {"Okay"})
+    end
+    
+    --Remove the ads
+    if (productName=="removeAds") then removeAds() end
+    
+    --Save any inventory changes
+    iap.saveInventory()
+    
+end
+
+--Restore function
+--Most of the code in this function places a spinner on screen to prevent any further user interaction with
+--the screen.  The actual code to initiate the purchase is the single line iap.restore(false, ...)
+local function restorePurchases()
+    
+    --Place a progress spinner on screen and tell the user the app is contating the store
+    local spinnerBackground = display.newRect(160,240,360,600)
+    spinnerBackground:setFillColor(1,1,1,0.75)
+    --Spinner consumes all taps so the user cannot tap the purchase button twice
+    spinnerBackground:addEventListener("tap", function() return true end)
+    local spinnerText = display.newText("Contacting " .. iap.getStoreName() .. "...", 160,180, native.systemFont, 18)
+    spinnerText:setFillColor(0,0,0)
+    --Add a little spinning rectangle
+    local spinnerRect = display.newRect(160,260,35,35)
+    spinnerRect:setFillColor(0.25,0.25,0.25)
+    transition.to(spinnerRect, { time=4000, rotation=360, iterations=999999, transition=easing.inOutQuad})
+    --Create a group and add all these objects to it
+    spinner=display.newGroup()
+    spinner:insert(spinnerBackground)
+    spinner:insert(spinnerText)
+    spinner:insert(spinnerRect)
+    
+    --Tell IAP to initiate a purchase
+    --Use the failedListener from onPurchase, which just clears away the spinner from the screen.
+    --You could have a separate function that tells the user "Unable to contact the app store" or
+    --similar on a timeout.
+    --On the simulator, or in debug mode, this function attempts to restore all of the non-consumable
+    --items in the catalogue.
+    iap.restore(false, restoreListener, failedListener)
+    
+end
+
 
 ---------------------------------
 -- 
@@ -155,6 +240,17 @@ else
     buyGroup:addEventListener("tap", buyUnlock)
 end
     
+--Draw "restore" button
+    --Create button background
+    local restoreBackground = display.newRect(160, 330, 180, 50)
+    restoreBackground.stroke = { 0.5, 0.5, 0.5 }
+    restoreBackground.strokeWidth = 2
+    --Create "buy IAP" text object
+    local restoreText = display.newText("Restore purchases", restoreBackground.x, restoreBackground.y, native.systemFont, 18)
+    restoreText:setFillColor(0,0,0)
+    --Add event listener
+    restoreText:addEventListener("tap", restorePurchases)
+
 --If the user hasn't unlocked the game, display an advertisement across the top of the screen
 if (iap.getInventoryValue("unlock")~=true) then
     --Create button background
